e2787d35e6c0a25d4a903c77f564d41897745243
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2026-08-25 13:39:42 +0300

    examples(out_gstdout): update example code to use new registration flow
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>

--- examples/out_gstdout/out_gstdout.go
+++ examples/out_gstdout/out_gstdout.go
@@ -11,7 +11,7 @@ import (
 
 //export FLBPluginRegister
 func FLBPluginRegister(def unsafe.Pointer) int {
-	return output.FLBPluginRegister(def, "gstdout", "Stdout GO!")
+	return output.FLBPluginRegister(def, output.WithName("gstdout"), output.WithDescription("Stdout GO!"))
 }
 
 //export FLBPluginInit
